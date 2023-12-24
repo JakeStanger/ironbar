JakeStanger/ironbar

commit bea442ed960f513288cf857e8ee9a5c61f742dfa

refactor: update gtk/glib, remove glib channels
This is a major refactor which updates GTK, GLib and GTK Layer Shell to their latest versions.

GLib channels, previously used for receiving events on the GLib Main Context thread have been deprecated and a new method for running Futures on the main thread has been added instead. This commit also replaces all the deprecated code with this.

As part of the above, a bug was uncovered related to creating the GLib main context inside the Tokio runtime. Spawning of Tokio tasks has been refactored to fix this.

src/main.rs
@@ -7,9 +7,9 @@ use std::path::PathBuf;
 use std::process::exit;
 use std::rc::Rc;
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
-use std::sync::mpsc;
 #[cfg(feature = "ipc")]
-use std::sync::{Arc, RwLock};
+use std::sync::RwLock;
+use std::sync::{mpsc, Arc};
 
 use cfg_if::cfg_if;
 #[cfg(feature = "cli")]
@@ -21,8 +21,8 @@ use glib::PropertySet;
 use gtk::gdk::Display;
 use gtk::prelude::*;
 use gtk::Application;
-use tokio::runtime::Handle;
-use tokio::task::{block_in_place, spawn_blocking};
+use tokio::runtime::{Handle, Runtime};
+use tokio::task::{block_in_place, JoinHandle};
 use tracing::{debug, error, info, warn};
 use universal_config::ConfigLoader;
 
@@ -36,7 +36,6 @@ use crate::ironvar::VariableManager;
 use crate::style::load_css;
 
 mod bar;
-mod bridge_channel;
 #[cfg(feature = "cli")]
 mod cli;
 mod clients;
@@ -60,13 +59,12 @@ mod style;
 const GTK_APP_ID: &str = "dev.jstanger.ironbar";
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let _guard = logging::install_logging();
 
     cfg_if! {
         if #[cfg(feature = "cli")] {
-            run_with_args().await;
+            run_with_args();
         } else {
             start_ironbar();
         }
@@ -74,22 +72,29 @@ async fn main() {
 }
 
 #[cfg(feature = "cli")]
-async fn run_with_args() {
+fn run_with_args() {
     let args = cli::Args::parse();
 
     match args.command {
         Some(command) => {
-            let ipc = ipc::Ipc::new();
-            match ipc.send(command).await {
-                Ok(res) => cli::handle_response(res),
-                Err(err) => error!("{err:?}"),
-            };
+            let rt = create_runtime();
+            rt.block_on(async move {
+                let ipc = ipc::Ipc::new();
+                match ipc.send(command).await {
+                    Ok(res) => cli::handle_response(res),
+                    Err(err) => error!("{err:?}"),
+                };
+            });
         }
         None => start_ironbar(),
     }
 }
 
 static COUNTER: AtomicUsize = AtomicUsize::new(1);
+
+lazy_static::lazy_static! {
+    static ref RUNTIME: Arc<Runtime> = Arc::new(create_runtime());
+}
 
 #[cfg(feature = "ipc")]
 lazy_static::lazy_static! {
@@ -182,6 +187,12 @@ impl Ironbar {
         // Ignore CLI args
         // Some are provided by swaybar_config but not currently supported
         app.run_with_args(&Vec::<&str>::new());
+    }
+
+    /// Gets the current Tokio runtime.
+    #[must_use]
+    pub fn runtime() -> Arc<Runtime> {
+        RUNTIME.clone()
     }
 
     /// Gets a `usize` ID value that is unique to the entire Ironbar instance.
@@ -321,6 +332,31 @@ fn create_bars(app: &Application, display: &Display, config: &Config) -> Result<
     }
 
     Ok(all_bars)
+}
+
+fn create_runtime() -> Runtime {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("tokio to create a valid runtime")
+}
+
+/// Calls `spawn` on the Tokio runtime.
+pub fn spawn<F>(f: F) -> JoinHandle<F::Output>
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    Ironbar::runtime().spawn(f)
+}
+
+/// Calls `spawn_blocking` on the Tokio runtime.
+pub fn spawn_blocking<F, R>(f: F) -> JoinHandle<R>
+where
+    F: FnOnce() -> R + Send + 'static,
+    R: Send + 'static,
+{
+    Ironbar::runtime().spawn_blocking(f)
 }
 
 /// Blocks on a `Future` until it resolves.

Cargo.toml
@@ -63,9 +63,9 @@ workspaces = ["futures-util"]
 
 [dependencies]
 # core
-gtk = "0.17.0"
-gtk-layer-shell = "0.6.0"
-glib = "0.17.10"
+gtk = "0.18.1"
+gtk-layer-shell = "0.8.0"
+glib = "0.18.4"
 tokio = { version = "1.35.0", features = [
   "macros",
   "rt-multi-thread",

src/bar.rs
@@ -5,9 +5,11 @@ use crate::modules::{
 use crate::popup::Popup;
 use crate::{Config, Ironbar};
 use color_eyre::Result;
+use glib::Propagation;
 use gtk::gdk::Monitor;
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, IconTheme, Orientation, Window, WindowType};
+use gtk_layer_shell::LayerShell;
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::time::Duration;
@@ -81,7 +83,7 @@ impl Bar {
         window.connect_destroy_event(|_, _| {
             info!("Shutting down");
             gtk::main_quit();
-            Inhibit(false)
+            Propagation::Proceed
         });
 
         Bar {
@@ -161,42 +163,38 @@ impl Bar {
     ) {
         let position = self.position;
 
-        gtk_layer_shell::init_for_window(win);
-        gtk_layer_shell::set_monitor(win, monitor);
-        gtk_layer_shell::set_layer(win, gtk_layer_shell::Layer::Top);
-        gtk_layer_shell::set_namespace(win, env!("CARGO_PKG_NAME"));
+        win.init_layer_shell();
+        win.set_monitor(monitor);
+        win.set_layer(gtk_layer_shell::Layer::Top);
+        win.set_namespace(env!("CARGO_PKG_NAME"));
 
         if exclusive_zone {
-            gtk_layer_shell::auto_exclusive_zone_enable(win);
+            win.auto_exclusive_zone_enable();
         }
 
-        gtk_layer_shell::set_margin(win, gtk_layer_shell::Edge::Top, margin.top);
-        gtk_layer_shell::set_margin(win, gtk_layer_shell::Edge::Bottom, margin.bottom);
-        gtk_layer_shell::set_margin(win, gtk_layer_shell::Edge::Left, margin.left);
-        gtk_layer_shell::set_margin(win, gtk_layer_shell::Edge::Right, margin.right);
+        win.set_layer_shell_margin(gtk_layer_shell::Edge::Top, margin.top);
+        win.set_layer_shell_margin(gtk_layer_shell::Edge::Bottom, margin.bottom);
+        win.set_layer_shell_margin(gtk_layer_shell::Edge::Left, margin.left);
+        win.set_layer_shell_margin(gtk_layer_shell::Edge::Right, margin.right);
 
         let bar_orientation = position.get_orientation();
 
-        gtk_layer_shell::set_anchor(
-            win,
+        win.set_anchor(
             gtk_layer_shell::Edge::Top,
             position == BarPosition::Top
                 || (bar_orientation == Orientation::Vertical && anchor_to_edges),
         );
-        gtk_layer_shell::set_anchor(
-            win,
+        win.set_anchor(
             gtk_layer_shell::Edge::Bottom,
             position == BarPosition::Bottom
                 || (bar_orientation == Orientation::Vertical && anchor_to_edges),
         );
-        gtk_layer_shell::set_anchor(
-            win,
+        win.set_anchor(
             gtk_layer_shell::Edge::Left,
             position == BarPosition::Left
                 || (bar_orientation == Orientation::Horizontal && anchor_to_edges),
         );
-        gtk_layer_shell::set_anchor(
-            win,
+        win.set_anchor(
             gtk_layer_shell::Edge::Right,
             position == BarPosition::Right
                 || (bar_orientation == Orientation::Horizontal && anchor_to_edges),
@@ -221,7 +219,7 @@ impl Bar {
                     win.hide();
                     hotspot_window.show();
                 });
-                Inhibit(false)
+                Propagation::Proceed
             });
         }
 
@@ -232,7 +230,7 @@ impl Bar {
                 hotspot_win.hide();
                 win.show();
 
-                Inhibit(false)
+                Propagation::Proceed
             });
         }
     }

src/bridge_channel.rs
@@ -1,44 +1,0 @@
-use crate::send;
-use tokio::spawn;
-use tokio::sync::mpsc;
-
-/// MPSC async -> GTK sync channel.
-/// The sender uses `tokio::sync::mpsc`
-/// while the receiver uses `glib::MainContext::channel`.
-///
-/// This makes it possible to send events asynchronously
-/// and receive them on the main thread,
-/// allowing UI updates to be handled on the receiving end.
-pub struct BridgeChannel<T> {
-    async_tx: mpsc::Sender<T>,
-    sync_rx: glib::Receiver<T>,
-}
-
-impl<T: Send + 'static> BridgeChannel<T> {
-    /// Creates a new channel
-    pub fn new() -> Self {
-        let (async_tx, mut async_rx) = mpsc::channel(32);
-        let (sync_tx, sync_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-
-        spawn(async move {
-            while let Some(val) = async_rx.recv().await {
-                send!(sync_tx, val);
-            }
-        });
-
-        Self { async_tx, sync_rx }
-    }
-
-    /// Gets a clone of the sender.
-    pub fn create_sender(&self) -> mpsc::Sender<T> {
-        self.async_tx.clone()
-    }
-
-    /// Attaches a callback to the receiver.
-    pub fn recv<F>(self, f: F) -> glib::SourceId
-    where
-        F: FnMut(T) -> glib::Continue + 'static,
-    {
-        self.sync_rx.attach(None, f)
-    }
-}

src/clients/clipboard.rs
@@ -1,10 +1,9 @@
 use super::wayland::{self, ClipboardItem};
-use crate::{arc_mut, lock, try_send};
+use crate::{arc_mut, lock, spawn, try_send};
 use indexmap::map::Iter;
 use indexmap::IndexMap;
 use lazy_static::lazy_static;
 use std::sync::{Arc, Mutex};
-use tokio::spawn;
 use tokio::sync::mpsc;
 use tracing::{debug, trace};
 

src/clients/compositor/hyprland.rs
@@ -1,5 +1,5 @@
 use super::{Visibility, Workspace, WorkspaceClient, WorkspaceUpdate};
-use crate::{arc_mut, lock, send};
+use crate::{arc_mut, lock, send, spawn_blocking};
 use color_eyre::Result;
 use hyprland::data::{Workspace as HWorkspace, Workspaces};
 use hyprland::dispatch::{Dispatch, DispatchType, WorkspaceIdentifierWithSpecial};
@@ -8,7 +8,6 @@ use hyprland::prelude::*;
 use hyprland::shared::{HyprDataVec, WorkspaceType};
 use lazy_static::lazy_static;
 use tokio::sync::broadcast::{channel, Receiver, Sender};
-use tokio::task::spawn_blocking;
 use tracing::{debug, error, info};
 
 pub struct EventClient {

src/clients/compositor/sway.rs
@@ -1,12 +1,11 @@
 use super::{Visibility, Workspace, WorkspaceClient, WorkspaceUpdate};
-use crate::{await_sync, send};
+use crate::{await_sync, send, spawn};
 use async_once::AsyncOnce;
 use color_eyre::Report;
 use futures_util::StreamExt;
 use lazy_static::lazy_static;
 use std::sync::Arc;
 use swayipc_async::{Connection, Event, EventType, Node, WorkspaceChange, WorkspaceEvent};
-use tokio::spawn;
 use tokio::sync::broadcast::{channel, Receiver, Sender};
 use tokio::sync::Mutex;
 use tracing::{info, trace};

src/clients/music/mpd.rs
@@ -1,7 +1,7 @@
 use super::{
     MusicClient, PlayerState, PlayerUpdate, ProgressTick, Status, Track, TICK_INTERVAL_MS,
 };
-use crate::{await_sync, send};
+use crate::{await_sync, send, spawn};
 use color_eyre::Result;
 use lazy_static::lazy_static;
 use mpd_client::client::{Connection, ConnectionEvent, Subsystem};
@@ -17,7 +17,6 @@ use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::net::{TcpStream, UnixStream};
-use tokio::spawn;
 use tokio::sync::broadcast;
 use tokio::sync::Mutex;
 use tokio::time::sleep;

commit 80de5dd824011b0eabf573cf4546c8e59b251bf7

fix: some modules crashing due to recent gtk refactor
Fixes a crash introduced by commit bea442e where the `await_sync` function incorrectly tried to use the current tokio runtime, which it is often outside, instead of the singleton.

Fixes #382

src/main.rs
@@ -21,7 +21,7 @@ use glib::PropertySet;
 use gtk::gdk::Display;
 use gtk::prelude::*;
 use gtk::Application;
-use tokio::runtime::{Handle, Runtime};
+use tokio::runtime::Runtime;
 use tokio::task::{block_in_place, JoinHandle};
 use tracing::{debug, error, info, warn};
 use universal_config::ConfigLoader;
@@ -370,5 +370,5 @@ where
 ///
 /// TODO: remove all instances of this once async trait funcs are stable
 pub fn await_sync<F: Future>(f: F) -> F::Output {
-    block_in_place(|| Handle::current().block_on(f))
+    block_in_place(|| Ironbar::runtime().block_on(f))
 }