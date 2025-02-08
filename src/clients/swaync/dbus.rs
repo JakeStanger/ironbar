63f59548379dfc56a0805bc8f5161c27e6175b44
Jake Stanger <mail@jstanger.dev>
2025-02-08 01:54:17 +0000

    refactor: upgrade to zbus v5
    Also drops the deprecated `upower-dbus` crate

--- src/clients/swaync/dbus.rs
+++ src/clients/swaync/dbus.rs
@@ -20,12 +20,14 @@
 //! [Writing a client proxy]: https://dbus2.github.io/zbus/client.html
 //! [D-Bus standard interfaces]: https://dbus.freedesktop.org/doc/dbus-specification.html#standard-interfaces,
 
-#[zbus::dbus_proxy(
+use zbus::proxy;
+
+#[proxy(
     interface = "org.erikreider.swaync.cc",
     default_service = "org.erikreider.swaync.cc",
     default_path = "/org/erikreider/swaync/cc"
 )]
-trait SwayNc {
+pub trait SwayNc {
     /// AddInhibitor method
     fn add_inhibitor(&self, application_id: &str) -> zbus::Result<bool>;
 
@@ -90,11 +92,11 @@ trait SwayNc {
     fn toggle_visibility(&self) -> zbus::Result<()>;
 
     /// Subscribe signal
-    #[dbus_proxy(signal)]
+    #[zbus(signal)]
     fn subscribe(&self, count: u32, dnd: bool, cc_open: bool) -> zbus::Result<()>;
 
     /// SubscribeV2 signal
-    #[dbus_proxy(signal)]
+    #[zbus(signal)]
     fn subscribe_v2(
         &self,
         count: u32,
@@ -104,8 +106,8 @@ trait SwayNc {
     ) -> zbus::Result<()>;
 
     /// Inhibited property
-    #[dbus_proxy(property)]
+    #[zbus(property)]
     fn inhibited(&self) -> zbus::Result<bool>;
-    #[dbus_proxy(property)]
+    #[zbus(property)]
     fn set_inhibited(&self, value: bool) -> zbus::Result<()>;
 }
--- src/clients/upower/mod.rs
+++ src/clients/upower/mod.rs
@@ -1,8 +1,14 @@
+mod device;
+mod upower;
+
 use crate::clients::ClientResult;
 use crate::register_fallible_client;
 use std::sync::Arc;
-use upower_dbus::UPowerProxy;
+use upower::UPowerProxy;
 use zbus::fdo::PropertiesProxy;
+use zbus::proxy::CacheProperties;
+
+pub use device::BatteryState;
 
 pub async fn create_display_proxy() -> ClientResult<PropertiesProxy<'static>> {
     let dbus = Box::pin(zbus::Connection::system()).await?;
@@ -11,14 +17,14 @@ pub async fn create_display_proxy() -> ClientResult<PropertiesProxy<'static>> {
 
     let display_device = device_proxy.get_display_device().await?;
 
-    let path = display_device.path().to_owned();
+    let path = display_device.inner().path();
 
     let proxy = PropertiesProxy::builder(&dbus)
         .destination("org.freedesktop.UPower")
         .expect("failed to set proxy destination address")
         .path(path)
         .expect("failed to set proxy path")
-        .cache_properties(zbus::CacheProperties::No)
+        .cache_properties(CacheProperties::No)
         .build()
         .await?;
 
