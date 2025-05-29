4c80218aba1ecda0d10d946057926c44f489ac4f
atagen <boss@atagen.co>
2025-05-29 23:35:29 +1000

    chore(nix): simplify packaging and outputs

--- nix/module.nix
+++ nix/module.nix
@@ -7,57 +7,65 @@ self: {
   cfg = config.programs.ironbar;
   defaultIronbarPackage = self.packages.${pkgs.hostPlatform.system}.default;
   jsonFormat = pkgs.formats.json {};
+  inherit
+    (lib)
+    types
+    mkOption
+    mkEnableOption
+    mkIf
+    getExe
+    ;
 in {
   options.programs.ironbar = {
-    enable = lib.mkEnableOption "ironbar status bar";
+    enable = mkEnableOption "ironbar status bar";
 
-    package = lib.mkOption {
-      type = with lib.types; package;
+    package = mkOption {
+      type = types.package;
       default = defaultIronbarPackage;
       apply = pkg: pkg.override {features = cfg.features;};
       description = "The package for ironbar to use.";
     };
 
-    systemd = lib.mkEnableOption "systemd service for ironbar.";
+    systemd = mkEnableOption "systemd service for ironbar.";
 
-    style = lib.mkOption {
-      type = lib.types.either (lib.types.lines) (lib.types.path);
+    style = mkOption {
+      type = types.either (types.lines) (types.path);
       default = "";
       description = "The stylesheet to apply to ironbar.";
     };
 
-    config = lib.mkOption {
+    config = mkOption {
       type = jsonFormat.type;
       default = {};
       description = "The config to pass to ironbar.";
     };
 
-    features = lib.mkOption {
-      type = lib.types.listOf lib.types.nonEmptyStr;
+    features = mkOption {
+      type = types.listOf types.nonEmptyStr;
       default = [];
       description = "The features to be used.";
     };
   };
 
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
     home.packages = [
       cfg.package
     ];
 
     xdg.configFile = {
-      "ironbar/config.json" = lib.mkIf (cfg.config != "") {
-        onChange = "${lib.getExe cfg.package} reload";
+      "ironbar/config.json" = mkIf (cfg.config != "") {
+        onChange = "${getExe cfg.package} reload";
         source = jsonFormat.generate "ironbar-config" cfg.config;
       };
 
-      "ironbar/style.css" = lib.mkIf (cfg.style != "") (
+      "ironbar/style.css" = mkIf (cfg.style != "") (
         if builtins.isPath cfg.style || lib.isStorePath cfg.style
         then {source = cfg.style;}
         else {text = cfg.style;}
       );
     };
 
-    systemd.user.services.ironbar = lib.mkIf cfg.systemd {
+    systemd.user.services.ironbar = mkIf cfg.systemd {
       Unit = {
         Description = "Systemd service for Ironbar";
         Documentation = "https://github.com/JakeStanger/ironbar";
@@ -70,8 +78,8 @@ in {
       };
 
       Service = {
-        ExecReload = "${lib.getExe cfg.package} reload";
-        ExecStart = "${lib.getExe cfg.package}";
+        ExecReload = "${getExe cfg.package} reload";
+        ExecStart = "${getExe cfg.package}";
         KillMode = "mixed";
         Restart = "on-failure";
       };
@@ -79,9 +87,9 @@ in {
       Install.WantedBy = [
         config.wayland.systemd.target
         "tray.target"
-        (lib.mkIf config.wayland.windowManager.hyprland.enable "hyprland-session.target")
-        (lib.mkIf config.wayland.windowManager.sway.enable "sway-session.target")
-        (lib.mkIf config.wayland.windowManager.river.enable "river-session.target")
+        (mkIf config.wayland.windowManager.hyprland.enable "hyprland-session.target")
+        (mkIf config.wayland.windowManager.sway.enable "sway-session.target")
+        (mkIf config.wayland.windowManager.river.enable "river-session.target")
       ];
     };
   };
