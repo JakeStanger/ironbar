c7acbba6b08fd1cc578861885804291e62c20003
Jake Stanger <mail@jstanger.dev>
2025-09-16 16:18:12 +0100

    build(docker): add github cli

--- .github/scripts/ubuntu_setup.sh
+++ .github/scripts/ubuntu_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # sudo needed for github runner, not available by default for cross images
 if command -v sudo >/dev/null 2>&1; then
@@ -21,3 +21,14 @@ $SUDO apt-get update && $SUDO apt-get install --assume-yes \
 	libdbusmenu-gtk3-dev${CROSS_DEB_ARCH:+:$CROSS_DEB_ARCH} \
 	libpulse-dev${CROSS_DEB_ARCH:+:$CROSS_DEB_ARCH} \
 	libluajit-5.1-dev${CROSS_DEB_ARCH:+:$CROSS_DEB_ARCH}
+
+# GH CLI, required by some CI jobs
+(type -p wget >/dev/null || ($SUDO apt update && $SUDO apt install wget -y)) \
+	&& $SUDO mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat "$out" | $SUDO tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& $SUDO chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& $SUDO mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | $SUDO tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& $SUDO apt update \
+	&& $SUDO apt install gh -y
