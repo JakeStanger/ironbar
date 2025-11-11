efece1b58aaf0eaa31fedadfb450a149364e257a
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2025-11-11 16:27:21 -0300

    style: move comments of unmapped dbus properties
    So they don't merge into doc-comments, making them harder to read.

--- src/clients/networkmanager/state.rs
+++ src/clients/networkmanager/state.rs
@@ -5,26 +5,34 @@ use super::dbus::{DeviceState, DeviceType};
 #[derive(Clone, Debug)]
 pub struct Device {
     pub path: ObjectPath<'static>,
-    // Udi                   readable   s
     /// Interface             readable   s
     pub interface: String,
+
+    /// State                 readable   u
+    ///
+    /// The current state of the device.
+    pub state: DeviceState,
+
+    /// Ip4Config             readable   o
+    ///
+    /// Object path of the Ip4Config object describing the configuration of the device. Only valid
+    /// when the device is in the NM_DEVICE_STATE_ACTIVATED state.
+    pub ip4_config: Option<Ip4Config>,
+
+    /// DeviceType            readable   u
+    ///
+    /// The general type of the network device; ie Ethernet, WiFi, etc.
+    pub device_type: DeviceType,
+    pub device_type_data: DeviceTypeData,
+    // Udi                   readable   s
     // IpInterface           readable   s
     // Driver                readable   s
     // DriverVersion         readable   s
     // FirmwareVersion       readable   s
     // Capabilities          readable   u
     // Ip4Address            readable   u
-    /// State                 readable   u
-    ///
-    /// The current state of the device.
-    pub state: DeviceState,
     // StateReason           readable   (uu)
     // ActiveConnection      readable   o
-    /// Ip4Config             readable   o
-    ///
-    /// Object path of the Ip4Config object describing the configuration of the device. Only valid
-    /// when the device is in the NM_DEVICE_STATE_ACTIVATED state.
-    pub ip4_config: Option<Ip4Config>,
     // Dhcp4Config           readable   o
     // Ip6Config             readable   o
     // Dhcp6Config           readable   o
@@ -32,11 +40,6 @@ pub struct Device {
     // Autoconnect           readwrite  b
     // FirmwareMissing       readable   b
     // NmPluginMissing       readable   b
-    /// DeviceType            readable   u
-    ///
-    /// The general type of the network device; ie Ethernet, WiFi, etc.
-    pub device_type: DeviceType,
-    pub device_type_data: DeviceTypeData,
     // AvailableConnections  readable   ao
     // PhysicalPortId        readable   s
     // Mtu                   readable   u
@@ -48,12 +51,12 @@ pub struct Device {
 #[derive(Clone, Debug)]
 pub struct Ip4Config {
     pub path: ObjectPath<'static>,
-    // Addresses    readable   aau
     /// AddressData  readable   aa{sv}
     ///
     /// Array of IP address data objects. All addresses will include "address" (an IP address
     /// string), and "prefix" (a uint). Some addresses may include additional attributes.
     pub address_data: Vec<AddressData>,
+    // Addresses    readable   aau
     // Gateway      readable   s
     // Routes       readable   aau
     // RouteData    readable   aa{sv}
@@ -67,9 +70,7 @@ pub struct Ip4Config {
 
 #[derive(Clone, Debug)]
 pub struct AddressData {
-    // address  s
     pub address: String,
-    // prefix   u
     pub prefix: u32,
 }
 
@@ -83,35 +84,36 @@ pub enum DeviceTypeData {
 
 #[derive(Clone, Debug)]
 pub struct DeviceWireless {
+    /// ActiveAccessPoint     readable   o
+    ///
+    /// Object path of the access point currently used by the wireless device.
+    pub active_access_point: Option<AccessPoint>,
     // HwAddress             readable   s
     // PermHwAddress         readable   s
     // Mode                  readable   u
     // Bitrate               readable   u
     // AccessPoints          readable   ao
-    /// ActiveAccessPoint     readable   o
-    ///
-    /// Object path of the access point currently used by the wireless device.
-    pub active_access_point: Option<AccessPoint>,
     // WirelessCapabilities  readable   u
 }
 
 #[derive(Clone, Debug)]
 pub struct AccessPoint {
     pub path: ObjectPath<'static>,
-    // Flags       readable   u
-    // WpaFlags    readable   u
-    // RsnFlags    readable   u
     /// Ssid        readable   ay
     ///
     /// The Service Set Identifier identifying the access point.
     pub ssid: Vec<u8>,
-    // Frequency   readable   u
-    // HwAddress   readable   s
-    // Mode        readable   u
-    // MaxBitrate  readable   u
+
     /// Strength    readable   y
     ///
     /// The current signal quality of the access point, in percent.
     pub strength: u8,
+    // Frequency   readable   u
+    // HwAddress   readable   s
+    // Mode        readable   u
+    // MaxBitrate  readable   u
+    // Flags       readable   u
+    // WpaFlags    readable   u
+    // RsnFlags    readable   u
     // LastSeen    readable   i
 }
