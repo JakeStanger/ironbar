JakeStanger/ironbar

commit 0bfa479c1ebab44511ac36861977db1723ce8cc9

fix: potential race condition on monitor connection causing issues with bar creation (#1198)
* fix(bar) destroy window when Bar is dropped

Fixes #1196

This exposes but does not yet address the window *creation* issues mentioned in
https://github.com/JakeStanger/ironbar/issues/1196#issuecomment-3405952395.

* fix(outputs): ensure monitor is available in GDK before creating bar

Issue becomes visible after fixing #1196.

See:

* https://github.com/JakeStanger/ironbar/issues/1196#issuecomment-3405952395
* https://github.com/JakeStanger/ironbar/issues/1196#issuecomment-3406603995

* fix(outputs): compilation with --no-default-features

* fix(outputs): implement code review feedback

src/bar.rs
@@ -40,6 +40,13 @@ pub struct Bar {
     inner: Inner,
 }
 
+impl Drop for Bar {
+    fn drop(&mut self) {
+        self.window.close();
+        self.window.destroy();
+    }
+}
+
 impl Bar {
     pub fn new(
         app: &Application,

src/main.rs
@@ -27,7 +27,7 @@ use universal_config::ConfigLoader;
 use crate::bar::{Bar, create_bar};
 use crate::channels::SyncSenderExt;
 use crate::clients::Clients;
-use crate::clients::wayland::OutputEventType;
+use crate::clients::outputs::MonitorState;
 use crate::config::{Config, MonitorConfig};
 use crate::desktop_file::DesktopFiles;
 use crate::error::ExitCode;
@@ -172,7 +172,6 @@ impl Ironbar {
 
         // force start wayland client ahead of ui
         let wl = instance.clients.borrow_mut().wayland();
-        let mut rx_outputs = wl.subscribe_outputs();
         wl.roundtrip();
 
         app.connect_activate(move |app| {
@@ -242,26 +241,40 @@ impl Ironbar {
                     .image_provider
                     .set_icon_theme(instance.config.borrow().icon_theme.as_deref());
 
+                // Load initial bars
+                match load_output_bars(&instance.clone(), &app) {
+                    Ok(_) => {}
+                    Err(report) => {
+                        error!("{:?}", report);
+                        exit(ExitCode::CreateBars as i32);
+                    }
+                };
+
+                let outputs = instance.clients.borrow_mut().outputs();
+                let mut rx_outputs = outputs.subscribe();
+
+                outputs.start(&instance.clone());
+
+                // Listen for monitor events
                 while let Ok(event) = rx_outputs.recv().await {
-                    match event.event_type {
-                        OutputEventType::New => {
-                            match load_output_bars(&instance, &app, &event.output) {
-                                Ok(mut new_bars) => {
-                                    instance.bars.borrow_mut().append(&mut new_bars);
-                                }
-                                Err(err) => error!("{err:?}"),
-                            }
-                        }
-                        OutputEventType::Destroyed => {
-                            let Some(name) = event.output.name else {
-                                continue;
-                            };
+                    match event.state {
+                        MonitorState::Disconnected => {
                             instance
                                 .bars
                                 .borrow_mut()
-                                .retain(|bar| bar.monitor_name() != name);
+                                .retain(|bar| bar.monitor_name() != event.connector);
                         }
-                        OutputEventType::Update => {}
+                        MonitorState::Connected(wl_output, gdk_output) => {
+                            if let Some(gdk_output) = gdk_output.upgrade() {
+                                match load_output_bars_for(&instance, &app, &wl_output, &gdk_output)
+                                {
+                                    Ok(mut new_bars) => {
+                                        instance.bars.borrow_mut().append(&mut new_bars);
+                                    }
+                                    Err(err) => error!("{err:?}"),
+                                }
+                            }
+                        }
                     }
                 }
             });
@@ -398,10 +411,11 @@ pub fn get_display() -> Display {
 }
 
 /// Loads all the bars associated with an output.
-fn load_output_bars(
+fn load_output_bars_for(
     ironbar: &Rc<Ironbar>,
     app: &Application,
     output: &OutputInfo,
+    monitor: &Monitor,
 ) -> Result<Vec<Bar>> {
     let Some(monitor_name) = &output.name else {
         return Err(Report::msg("Output missing monitor name"));
@@ -410,29 +424,6 @@ fn load_output_bars(
     let monitor_desc = &output.description.clone().unwrap_or_default();
 
     let config = ironbar.config.borrow();
-
-    let display = get_display();
-
-    let monitors = display.monitors();
-    let find_monitor = || {
-        for i in 0..monitors.n_items() {
-            let Some(monitor) = monitors.item(i).and_downcast::<Monitor>() else {
-                continue;
-            };
-
-            if monitor.description().unwrap_or_default().as_str() == monitor_desc
-                || monitor.connector().unwrap_or_default().as_str() == monitor_name
-            {
-                return Some(monitor);
-            }
-        }
-
-        None
-    };
-
-    let Some(monitor) = find_monitor() else {
-        return Err(Report::msg("failed to find matching monitor"));
-    };
 
     let show_default_bar =
         config.bar.start.is_some() || config.bar.center.is_some() || config.bar.end.is_some();
@@ -448,7 +439,7 @@ fn load_output_bars(
         Some(MonitorConfig::Single(config)) => {
             vec![create_bar(
                 app,
-                &monitor,
+                monitor,
                 monitor_name.to_string(),
                 config.clone(),
                 ironbar.clone(),
@@ -459,7 +450,7 @@ fn load_output_bars(
             .map(|config| {
                 create_bar(
                     app,
-                    &monitor,
+                    monitor,
                     monitor_name.to_string(),
                     config.clone(),
                     ironbar.clone(),
@@ -468,7 +459,7 @@ fn load_output_bars(
             .collect(),
         None if show_default_bar => vec![create_bar(
             app,
-            &monitor,
+            monitor,
             monitor_name.to_string(),
             config.bar.clone(),
             ironbar.clone(),
@@ -477,6 +468,48 @@ fn load_output_bars(
     };
 
     Ok(bars)
+}
+
+pub fn load_output_bars(ironbar: &Rc<Ironbar>, app: &Application) -> Result<()> {
+    let wl = ironbar.clients.borrow_mut().wayland();
+    let outputs = wl.output_info_all();
+
+    let display = crate::get_display();
+    let monitors = display.monitors();
+
+    for output in outputs {
+        let Some(monitor_name) = &output.name else {
+            return Err(Report::msg("Output missing monitor name"));
+        };
+        let monitor_desc = &output.description.clone().unwrap_or_default();
+        let find_monitor = || {
+            for i in 0..monitors.n_items() {
+                let Some(monitor) = monitors.item(i).and_downcast::<Monitor>() else {
+                    continue;
+                };
+
+                if monitor.description().unwrap_or_default().as_str() == monitor_desc
+                    || monitor.connector().unwrap_or_default().as_str() == monitor_name
+                {
+                    return Some(monitor);
+                }
+            }
+
+            None
+        };
+
+        let Some(monitor) = find_monitor() else {
+            error!("failed to find matching monitor for {}", monitor_name);
+            continue;
+        };
+
+        match crate::load_output_bars_for(ironbar, app, &output, &monitor) {
+            Ok(mut bars) => ironbar.bars.borrow_mut().append(&mut bars),
+            Err(err) => error!("{err:?}"),
+        }
+    }
+
+    Ok(())
 }
 
 fn create_runtime() -> Runtime {

src/clients/mod.rs
@@ -24,6 +24,7 @@ pub mod lua;
 pub mod music;
 #[cfg(feature = "network_manager")]
 pub mod networkmanager;
+pub mod outputs;
 #[cfg(feature = "sway")]
 pub mod sway;
 #[cfg(feature = "notifications")]
@@ -43,6 +44,7 @@ pub mod wayland;
 #[derive(Debug, Default)]
 pub struct Clients {
     wayland: Option<Arc<wayland::Client>>,
+    outputs: Option<Arc<outputs::Client>>,
     #[cfg(feature = "workspaces")]
     workspaces: Option<Arc<dyn compositor::WorkspaceClient>>,
     #[cfg(feature = "sway")]
@@ -87,6 +89,12 @@ impl Clients {
     pub fn wayland(&mut self) -> Arc<wayland::Client> {
         self.wayland
             .get_or_insert_with(|| Arc::new(wayland::Client::new()))
+            .clone()
+    }
+
+    pub fn outputs(&mut self) -> Arc<outputs::Client> {
+        self.outputs
+            .get_or_insert_with(|| Arc::new(outputs::Client::new()))
             .clone()
     }
 

src/clients/outputs.rs
@@ -1,0 +1,195 @@
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+
+use crate::channels::SyncSenderExt;
+use crate::clients::wayland;
+use crate::{Ironbar, arc_mut, debug, get_display, info, lock, register_client};
+use gtk::gdk::Monitor;
+use gtk::gdk::prelude::*;
+use smithay_client_toolkit::output::OutputInfo;
+use tokio::sync::broadcast::{self, Sender};
+
+#[derive(Debug, Clone)]
+pub enum MonitorState {
+    Disconnected,
+    Connected(OutputInfo, glib::SendWeakRef<Monitor>),
+}
+
+#[derive(Debug, Clone)]
+pub struct MonitorEvent {
+    pub connector: String,
+    pub state: MonitorState,
+}
+
+/// There are two ways in which we are notified of output events:
+/// 1. Through a wayland client event
+/// 2. Through a GDK event
+///
+/// We need to collate both events before we can call `load_output_bars`.
+enum InternalMonitorState {
+    Disconnected,
+    WaylandConnected(OutputInfo),
+    GdkConnected(glib::SendWeakRef<Monitor>),
+    BothConnected(OutputInfo, glib::SendWeakRef<Monitor>),
+}
+
+struct MonitorProxy {
+    pub connector: String,
+    state: InternalMonitorState,
+}
+
+impl MonitorProxy {
+    fn disconnect(&mut self) -> &mut Self {
+        self.state = InternalMonitorState::Disconnected;
+
+        self
+    }
+
+    fn connect_wayland(&mut self, wl_monitor: &OutputInfo) -> &mut Self {
+        self.state = match &self.state {
+            InternalMonitorState::GdkConnected(gdk_monitor) => {
+                InternalMonitorState::BothConnected(wl_monitor.clone(), gdk_monitor.clone())
+            }
+            _ => InternalMonitorState::WaylandConnected(wl_monitor.clone()),
+        };
+
+        self
+    }
+
+    fn connect_gdk(&mut self, gdk_monitor: glib::SendWeakRef<Monitor>) -> &mut Self {
+        self.state = match &self.state {
+            InternalMonitorState::WaylandConnected(wl_monitor) => {
+                InternalMonitorState::BothConnected(wl_monitor.clone(), gdk_monitor)
+            }
+            _ => InternalMonitorState::GdkConnected(gdk_monitor),
+        };
+
+        self
+    }
+
+    fn maybe_send(&self, tx: &Sender<MonitorEvent>) {
+        match &self.state {
+            InternalMonitorState::Disconnected => {
+                info!("Monitor {} disconnected", self.connector);
+                tx.send_expect(MonitorEvent {
+                    connector: self.connector.clone(),
+                    state: MonitorState::Disconnected,
+                })
+            }
+            InternalMonitorState::BothConnected(wl_output, gdk_output) => {
+                info!("Monitor {} connected", self.connector);
+                tx.send_expect(MonitorEvent {
+                    connector: self.connector.clone(),
+                    state: MonitorState::Connected(wl_output.clone(), gdk_output.clone()),
+                })
+            }
+            _ => {}
+        }
+    }
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct BroadcastChannel<T>(broadcast::Sender<T>, Arc<Mutex<broadcast::Receiver<T>>>);
+
+impl<T> From<(broadcast::Sender<T>, broadcast::Receiver<T>)> for BroadcastChannel<T> {
+    fn from(value: (broadcast::Sender<T>, broadcast::Receiver<T>)) -> Self {
+        Self(value.0, arc_mut!(value.1))
+    }
+}
+
+#[derive(Debug)]
+pub struct Client {
+    // Associates a connector ID (e.g. HDMI-1) to a state
+    output_channel: BroadcastChannel<MonitorEvent>,
+}
+
+impl Client {
+    pub(crate) fn new() -> Self {
+        Self {
+            output_channel: broadcast::channel(8).into(),
+        }
+    }
+
+    pub(crate) fn start(&self, ironbar: &Rc<Ironbar>) {
+        let mut rx_wl_outputs = ironbar.clients.borrow_mut().wayland().subscribe_outputs();
+
+        let monitors = arc_mut!(HashMap::new());
+
+        // listen to wayland events
+        {
+            let monitors = monitors.clone();
+            let output_tx = self.output_channel.0.clone();
+
+            glib::spawn_future_local(async move {
+                while let Ok(event) = rx_wl_outputs.recv().await {
+                    debug!("Wayland output event: {event:?}");
+                    if let Some(name) = &event.output.name {
+                        let mut guard = lock!(monitors);
+                        let entry = guard.entry(name.clone()).or_insert_with(|| MonitorProxy {
+                            connector: name.clone(),
+                            state: InternalMonitorState::Disconnected,
+                        });
+                        match event.event_type {
+                            wayland::OutputEventType::New => {
+                                entry.connect_wayland(&event.output).maybe_send(&output_tx)
+                            }
+                            wayland::OutputEventType::Destroyed => {
+                                entry.disconnect().maybe_send(&output_tx)
+                            }
+                            wayland::OutputEventType::Update => {}
+                        };
+                    }
+                }
+            });
+        }
+
+        // listen to GDK events
+        {
+            let output_tx = self.output_channel.0.clone();
+            let monitors = monitors.clone();
+
+            let display = get_display();
+            display
+                .monitors()
+                .connect_items_changed(move |list, position, removed, added| {
+                    debug!("GDK event: +{added}, -{removed}");
+                    for added_idx in position..position + added {
+                        if let Some(monitor) = list.item(added_idx).and_downcast::<Monitor>() {
+                            let output_tx = output_tx.clone();
+                            let monitors = monitors.clone();
+
+                            /*
+                             * At this point, we have a `gdk::Monitor`. However, the object initially
+                             * has all its fields `None`, including `connector`.
+                             *
+                             * We have to listen for the notify event for the `connector` being set.
+                             */
+                            monitor.connect_notify(Some("connector"), move |m, _| {
+                                if let Some(connector) = m.connector() {
+                                    lock!(monitors)
+                                        .entry(connector.to_string())
+                                        .or_insert_with(|| MonitorProxy {
+                                            connector: connector.to_string(),
+                                            state: InternalMonitorState::Disconnected,
+                                        })
+                                        .connect_gdk(glib::SendWeakRef::from(ObjectExt::downgrade(
+                                            m,
+                                        )))
+                                        .maybe_send(&output_tx);
+                                }
+                            });
+                        }
+                    }
+                });
+        }
+    }
+
+    /// Subscribes to events
+    pub fn subscribe(&self) -> broadcast::Receiver<MonitorEvent> {
+        self.output_channel.0.subscribe()
+    }
+}
+
+register_client!(Client, outputs);

src/clients/wayland/mod.rs
@@ -69,7 +69,6 @@ pub enum Event {
 pub enum Request {
     Roundtrip,
 
-    #[cfg(feature = "ipc")]
     OutputInfoAll,
 
     #[cfg(any(feature = "focused", feature = "launcher"))]
@@ -90,7 +89,6 @@ pub enum Response {
     /// An empty success response
     Ok,
 
-    #[cfg(feature = "ipc")]
     OutputInfoAll(Vec<smithay_client_toolkit::output::OutputInfo>),
 
     #[cfg(any(feature = "focused", feature = "launcher"))]
@@ -336,7 +334,6 @@ impl Environment {
                 debug!("received roundtrip request");
                 env.response_tx.send_expect(Response::Ok);
             }
-            #[cfg(feature = "ipc")]
             Msg(Request::OutputInfoAll) => {
                 let infos = env.output_info_all();
                 env.response_tx.send_expect(Response::OutputInfoAll(infos));

src/clients/wayland/wl_output.rs
@@ -21,7 +21,6 @@ pub enum OutputEventType {
 
 impl Client {
     /// Gets the information for all outputs.
-    #[cfg(feature = "ipc")]
     pub fn output_info_all(&self) -> Vec<OutputInfo> {
         use super::{Request, Response};
         match self.send_request(Request::OutputInfoAll) {
@@ -37,7 +36,6 @@ impl Client {
 }
 
 impl Environment {
-    #[cfg(feature = "ipc")]
     pub fn output_info_all(&mut self) -> Vec<OutputInfo> {
         self.output_state
             .outputs()

src/ipc/server/mod.rs
@@ -142,18 +142,12 @@ impl Ipc {
                     window.close();
                 }
 
-                let wl = ironbar.clients.borrow_mut().wayland();
-                let outputs = wl.output_info_all();
-
                 ironbar.reload_config();
 
-                for output in outputs {
-                    match crate::load_output_bars(ironbar, application, &output) {
-                        Ok(mut bars) => ironbar.bars.borrow_mut().append(&mut bars),
-                        Err(err) => error!("{err:?}"),
-                    }
+                match crate::load_output_bars(ironbar, application) {
+                    Ok(_) => {}
+                    Err(err) => error!("{err:?}"),
                 }
-
                 Response::Ok
             }
             Command::Var(cmd) => ironvar::handle_command(cmd),

commit 2c11d674247a55bf9f52b3ee67f5699b81b2af0f

fix: regression from #1198
Fixes #1248

src/bar.rs
@@ -40,13 +40,6 @@ pub struct Bar {
     inner: Inner,
 }
 
-impl Drop for Bar {
-    fn drop(&mut self) {
-        self.window.close();
-        self.window.destroy();
-    }
-}
-
 impl Bar {
     pub fn new(
         app: &Application,
@@ -162,6 +155,12 @@ impl Bar {
         };
 
         self
+    }
+
+    /// Closes the bar, consuming it.
+    pub fn close(self) {
+        self.window.close();
+        self.window.destroy();
     }
 
     /// Sets up GTK layer shell for a provided application window.

src/main.rs
@@ -249,7 +249,8 @@ impl Ironbar {
                             instance
                                 .bars
                                 .borrow_mut()
-                                .retain(|bar| bar.monitor_name() != event.connector);
+                                .extract_if(.., |bar| bar.monitor_name() == event.connector)
+                                .for_each(Bar::close);
                         }
                         MonitorState::Connected(wl_output, gdk_output) => {
                             if let Some(gdk_output) = gdk_output.upgrade() {
@@ -418,7 +419,7 @@ pub fn load_output_bars(ironbar: &Rc<Ironbar>, app: &Application) -> Result<()> 
     let wl = ironbar.clients.borrow_mut().wayland();
     let outputs = wl.output_info_all();
 
-    let display = crate::get_display();
+    let display = get_display();
     let monitors = display.monitors();
 
     for output in outputs {