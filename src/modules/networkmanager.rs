339787aad4f41137b84eda60e34180593766886f
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2026-02-11 19:36:45 -0300

    refactor: avoid clones by replacing recv_glib with recv_glib_async
    Before, a lot of values were being cloned in the async block returned by
    the recv_glib_async closure. But since we are now using
    glib::spawn_future_local (because the ProfileManager callback is not
    async), we can call recv_glib_async and use recv_glib.
    
    And actually, the cloned values were being used before the first .await
    in the async block, so we could have avoided those clones all along.

--- src/modules/networkmanager.rs
+++ src/modules/networkmanager.rs
@@ -222,96 +222,82 @@ impl Module<GtkBox> for NetworkManagerModule {
 
         let image_provider = context.ironbar.image_provider();
 
-        let container_clone = container.clone();
         let icon_size = self.icon_size;
-        context.subscribe().recv_glib_async((), move |(), update| {
-            let container = container.clone();
-            let image_provider = image_provider.clone();
-            let this = self.clone();
-
-            fn is_static<T: 'static>(x: T) -> T {
-                x
+        let mut manager = self.profiles.attach(&container, move |_, event| {
+            let (widget, image_provider): (gtk::Widget, Provider) = event.data;
+            let icon_name = event.profile.icon.clone();
+            tracing::debug!("profiles update: icon_name={icon_name}");
+            if icon_name.is_empty() {
+                widget.set_visible(false);
+                return;
             }
 
-            let manager = self.profiles.attach(&container, move |_, event| {
-                let (widget, image_provider): (gtk::Widget, Provider) = event.data;
-                let icon_name = event.profile.icon.clone();
-                tracing::debug!("profiles update: icon_name={icon_name}");
-                if icon_name.is_empty() {
-                    widget.set_visible(false);
-                    return;
-                }
-
-                glib::spawn_future_local(async move {
-                    image_provider
-                        .load_into_picture_silent(
-                            &icon_name,
-                            icon_size,
-                            false,
-                            widget.downcast_ref::<Picture>().expect("should be Picture"),
-                        )
-                        .await;
-                    widget.set_visible(true)
-                });
+            glib::spawn_future_local(async move {
+                image_provider
+                    .load_into_picture_silent(
+                        &icon_name,
+                        icon_size,
+                        false,
+                        widget.downcast_ref::<Picture>().expect("should be Picture"),
+                    )
+                    .await;
+                widget.set_visible(true)
             });
-            let mut manager = is_static(manager);
+        });
 
-            async move {
-                match update {
-                    NetworkManagerUpdate::Devices(devices) => {
-                        tracing::debug!("NetworkManager devices updated");
-                        tracing::trace!("NetworkManager devices updated: {devices:#?}");
+        let container_clone = container.clone();
+        context.subscribe().recv_glib((), move |(), update| {
+            match update {
+                NetworkManagerUpdate::Devices(devices) => {
+                    tracing::debug!("NetworkManager devices updated");
+                    tracing::trace!("NetworkManager devices updated: {devices:#?}");
 
-                        // resize the container's children to match the number of devices
-                        if container.children().count() > devices.len() {
-                            for child in container.children().skip(devices.len()) {
-                                container.remove(&child);
-                            }
-                        } else {
-                            while container.children().count() < devices.len() {
-                                let icon = Picture::builder()
-                                    .content_fit(ContentFit::ScaleDown)
-                                    .css_classes(["icon"])
-                                    .build();
-                                container.append(&icon);
-                            }
+                    // resize the container's children to match the number of devices
+                    if container.children().count() > devices.len() {
+                        for child in container.children().skip(devices.len()) {
+                            container.remove(&child);
                         }
-
-                        // update each icon to match the device state
-                        for (device, widget) in devices.iter().zip(container.children()) {
-                            match this.get_profile_state(device) {
-                                Some(state) => {
-                                    let tooltip = this.get_tooltip(device);
-                                    widget.set_tooltip_text(Some(&tooltip));
-                                    manager.update(state, (widget, image_provider.clone()));
-                                }
-                                _ => {
-                                    widget.set_visible(false);
-                                    continue;
-                                }
-                            };
+                    } else {
+                        while container.children().count() < devices.len() {
+                            let icon = Picture::builder()
+                                .content_fit(ContentFit::ScaleDown)
+                                .css_classes(["icon"])
+                                .build();
+                            container.append(&icon);
                         }
                     }
-                    NetworkManagerUpdate::Device(idx, device) => {
-                        tracing::debug!(
-                            "NetworkManager device {idx} updated: {}",
-                            device.interface
-                        );
-                        tracing::trace!("NetworkManager device {idx} updated: {device:#?}");
-                        if let Some(widget) = container.children().nth(idx) {
-                            match this.get_profile_state(&device) {
-                                Some(state) => {
-                                    let tooltip = this.get_tooltip(&device);
-                                    widget.set_tooltip_text(Some(&tooltip));
-                                    manager.update(state, (widget, image_provider.clone()));
-                                }
-                                _ => {
-                                    widget.set_visible(false);
-                                }
-                            };
-                        } else {
-                            tracing::warn!("No widget found for device index {idx}");
-                        }
+
+                    // update each icon to match the device state
+                    for (device, widget) in devices.iter().zip(container.children()) {
+                        match self.get_profile_state(device) {
+                            Some(state) => {
+                                let tooltip = self.get_tooltip(device);
+                                widget.set_tooltip_text(Some(&tooltip));
+                                manager.update(state, (widget, image_provider.clone()));
+                            }
+                            _ => {
+                                widget.set_visible(false);
+                                continue;
+                            }
+                        };
+                    }
+                }
+                NetworkManagerUpdate::Device(idx, device) => {
+                    tracing::debug!("NetworkManager device {idx} updated: {}", device.interface);
+                    tracing::trace!("NetworkManager device {idx} updated: {device:#?}");
+                    if let Some(widget) = container.children().nth(idx) {
+                        match self.get_profile_state(&device) {
+                            Some(state) => {
+                                let tooltip = self.get_tooltip(&device);
+                                widget.set_tooltip_text(Some(&tooltip));
+                                manager.update(state, (widget, image_provider.clone()));
+                            }
+                            _ => {
+                                widget.set_visible(false);
+                            }
+                        };
+                    } else {
+                        tracing::warn!("No widget found for device index {idx}");
                     }
                 }
             }
